3d79b827708270f898ef092f79984f4fe00d8266
Bogdan Petrukovich <viorbrint@gmail.com>
2025-01-22 23:31:10 +0300

    feat: npm init + tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/**/*.{html,css}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#170f1f",
+        text: "#e8d4fc",
+        primary: "#af97ce",
+        secondary: "#4c2c7d",
+        accent: "#54cdaa",
+        "text-accent": "#f2e8ff",
+      },
+      fontSize: {
+        base: "0.875rem",
+        lg: "1.15625rem",
+        xl: "1.5625rem",
+        "2xl": "1.9375rem",
+      },
+      fontFamily: {
+        main: "Inter",
+      },
+      boxShadow: {
+        "purple-haze": "0px 10px 50px 0px rgba(242,232,255,0.25)",
+      },
+      borderRadius: {
+        "rounded-0": "0rem",
+        "rounded-1": "1.1428571428571428rem",
+        "rounded-2": "3.5714285714285716rem",
+      },
+    },
+  },
+  plugins: [],
+};
