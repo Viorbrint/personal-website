8420eff656acdd388a84f175143eb6ade76d49e4
Bogdan Petrukovich <viorbrint@gmail.com>
2025-01-23 02:53:12 +0300

    feat: morgan

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import morgan from "morgan";
 
 const DEFAULT_PORT = 3000;
 const port = process.env.PORT || DEFAULT_PORT;
 
 const app = express();
+app.use(morgan("dev"));
 
 app.all("/*", (req, res) => {
   res.send("Hello World!!!");
