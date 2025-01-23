71c680885abbbbdb0b4fd2729ab4f07dad320e87
Bogdan Petrukovich <viorbrint@gmail.com>
2025-01-23 03:52:45 +0300

    feat: ejs + htmx

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import express from "express";
 import morgan from "morgan";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__dirname);
 
 const DEFAULT_PORT = 3000;
 const port = process.env.PORT || DEFAULT_PORT;
 
 const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.all("/*", (req, res) => {
-  res.send("Hello World!!!");
+let count = 0;
+
+app.get("/", (_, res) => {
+  res.render("index");
+});
+
+app.post("/clicked", (_, res) => {
+  res.send(String(count));
+  count++;
 });
 
 app
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["src/**/*.{html,css}"],
+  content: ["src/**/*.{ejs,css}"],
   theme: {
     extend: {
       colors: {
